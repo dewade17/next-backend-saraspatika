f128c5ef078c4a09f3dd332dd6d21062016dc1c6
dewade17 <andrenewer@gmail.com>
2026-01-20 15:15:35 +0800

    update: all-package-json

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,16 @@
-import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaClient } from '@prisma/client'; // Gunakan default import
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
+
 const g = globalThis;
-export const prisma = g.__db || new PrismaClient();
+
+const createPrismaClient = () => {
+  const connectionString = process.env.DATABASE_URL;
+  const pool = new pg.Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
+};
+
+export const prisma = g.__db || createPrismaClient();
+
 if (process.env.NODE_ENV !== 'production') g.__db = prisma;
